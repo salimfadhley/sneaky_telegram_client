64157a51066b72e17dfbfa711be68af67d34db42
Salim Fadhley <salimfadhley@gmail.com>
2021-01-17 18:57:40 +0000

    Tidy up code.

--- src/src/sneaky_client/message_digest.py
+++ src/src/sneaky_client/message_digest.py
@@ -5,10 +5,18 @@ import pprint
 import logging
 
 
-from telethon.tl.types import UpdateNewChannelMessage, User, Channel, MessageMediaDocument, MessageMediaWebPage, \
-    MessageMediaPhoto, MessageMediaContact
+from telethon.tl.types import (
+    UpdateNewChannelMessage,
+    User,
+    Channel,
+    MessageMediaDocument,
+    MessageMediaWebPage,
+    MessageMediaPhoto,
+    MessageMediaContact,
+)
 
 log = logging.getLogger(__name__)
+
 
 @dataclass
 class UserDigest:
@@ -45,24 +53,25 @@ class MessageDigest:
     id: int
 
 
-
-
 @dataclass
 class DocumentDigest:
     id: int
     access_hash: int
     size: int
 
+
 @dataclass
 class PhotoDigest:
     id: int
     access_hash: int
+
 
 @dataclass
 class WebPageDigest:
     id: int
     title: str
     url: str
+
 
 @dataclass
 class ContactDigest:
@@ -74,43 +83,46 @@ class ContactDigest:
 
 @dataclass
 class MediaDigest:
-    photo: Optional[PhotoDigest]=None
-    webpage: Optional[WebPageDigest]=None
-    contact: Optional[ContactDigest]=None
-    document: Optional[DocumentDigest]=None
+    photo: Optional[PhotoDigest] = None
+    webpage: Optional[WebPageDigest] = None
+    contact: Optional[ContactDigest] = None
+    document: Optional[DocumentDigest] = None
 
-def get_media_digest(media)->MediaDigest:
+
+def get_media_digest(media) -> MediaDigest:
     log.info(f"Got media: {media}")
     if not media:
         return None
     elif isinstance(media, MessageMediaDocument):
-        return MediaDigest(document=DocumentDigest(
-            id = media.document.id,
-            access_hash=media.document.access_hash,
-            size=media.document.size
-        ))
+        return MediaDigest(
+            document=DocumentDigest(
+                id=media.document.id,
+                access_hash=media.document.access_hash,
+                size=media.document.size,
+            )
+        )
     elif isinstance(media, MessageMediaWebPage):
-        return MediaDigest(webpage=WebPageDigest(
-            id=media.webpage.id,
-            title=media.webpage.title,
-            url=media.webpage.url
-        ))
+        return MediaDigest(
+            webpage=WebPageDigest(
+                id=media.webpage.id, title=media.webpage.title, url=media.webpage.url
+            )
+        )
     elif isinstance(media, MessageMediaPhoto):
-        return MediaDigest(photo=PhotoDigest(
-            id = media.photo.id,
-            access_hash=media.photo.access_hash
-        ))
+        return MediaDigest(
+            photo=PhotoDigest(id=media.photo.id, access_hash=media.photo.access_hash)
+        )
     elif isinstance(media, MessageMediaContact):
-        return MediaDigest(contact=ContactDigest(
-            user_id=media.contact.user_id,
-            first_name=media.contact.first_name,
-            last_name=media.contact.last_name,
-            phone_number=media.contact.phone_number
-        ))
+        return MediaDigest(
+            contact=ContactDigest(
+                user_id=media.contact.user_id,
+                first_name=media.contact.first_name,
+                last_name=media.contact.last_name,
+                phone_number=media.contact.phone_number,
+            )
+        )
     else:
         log.info(f"Cannot handle {media.__class__}: {pprint.pprint(media)}")
         return MediaDigest()
-
 
 
 @dataclass
@@ -120,36 +132,37 @@ class UpdateDigest:
     message: MessageDigest
     id: int
 
-def create_digest(update:UpdateNewChannelMessage, user:User, channel:Channel)->UpdateDigest:
+
+def create_digest(
+    update: UpdateNewChannelMessage, user: User, channel: Channel
+) -> UpdateDigest:
     channel_digest = ChannelDigest(
-        id = channel.id,
-        name = channel.title,
-        access_hash = channel.access_hash,
+        id=channel.id, name=channel.title, access_hash=channel.access_hash,
     )
 
     user_digest = UserDigest(
-        id = user.id,
+        id=user.id,
         name=user.username,
         first_name=user.first_name,
         last_name=user.last_name,
         phone=user.phone,
-        access_hash=user.access_hash
+        access_hash=user.access_hash,
     )
 
     message_digest = MessageDigest(
-        id = update.message.id,
-        message = update.message.message,
-        media = get_media_digest(update.message.media),
-        date = update.message.date
+        id=update.message.id,
+        message=update.message.message,
+        media=get_media_digest(update.message.media),
+        date=update.message.date,
     )
 
     hashable_identifiers = (channel_digest.id, user_digest.id, message_digest.id)
 
     update_digest = UpdateDigest(
-        user = user_digest,
-        channel = channel_digest,
-        message= message_digest,
-        id = hash(hashable_identifiers)
+        user=user_digest,
+        channel=channel_digest,
+        message=message_digest,
+        id=hash(hashable_identifiers),
     )
 
     return update_digest
--- src/src/sneaky_client/storage.py
+++ src/src/sneaky_client/storage.py
@@ -15,7 +15,7 @@ def get_elasticsearch() -> elasticsearch.Elasticsearch:
     return elasticsearch.Elasticsearch([{"host": "elastic", "port": 9200}])
 
 
-def store(update: UpdateNewChannelMessage, user:User, channel:Channel) -> None:
+def store(update: UpdateNewChannelMessage, user: User, channel: Channel) -> None:
     es = get_elasticsearch()
     update_as_dict = update.to_dict()
 
@@ -25,11 +25,12 @@ def store(update: UpdateNewChannelMessage, user:User, channel:Channel) -> None:
     update_id = hash((user.id, channel.id, update.message.id))
 
     es.index(index="updates", doc_type="update", id=update_id, body=update_as_dict)
-    es.index(index="channels", doc_type="channel", id=channel.id, body=channel.to_dict())
+    es.index(
+        index="channels", doc_type="channel", id=channel.id, body=channel.to_dict()
+    )
     es.index(index="users", doc_type="user", id=user.id, body=user.to_dict())
 
     digest = create_digest(user=user, channel=channel, update=update)
     digest_as_dict = dataclasses.asdict(digest)
     log.info(f"Saving message: {pprint.pformat(digest_as_dict)}")
     es.index(index="digests", doc_type="digest", id=digest.id, body=digest_as_dict)
-
